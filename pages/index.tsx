6dcef75b040799bcd0bf8a9259c9942802814d25
root <root@ip-172-31-21-113.ap-south-1.compute.internal>
2025-10-07 08:46:33 +0000

    Initial commit: minimal DevOps front page

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px", fontFamily: "Arial" }}>
+      <h1>DevOps Assignment</h1>
+      <p>This is the front page for my DevOps assignment.</p>
+    </div>
+  );
+}
+
